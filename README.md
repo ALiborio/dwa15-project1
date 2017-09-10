d1ab10bb5ad0b65edcf5f5bae32665d450e50d1e
Adam Liborio <AdamLiborio@gmail.com>
2017-09-09 23:16:52 -0400

    adding first draft index page, also updated README

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# dwa15-project1
-Project 1 for Dynamic Web Applications Fall 2017
+# Project 1 for Dynamic Web Applications Fall 2017
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Adam Liborio</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<h1>Adam Liborio</h1>
+<img src="images/adamliborio.jpg">
+<h2>About Me</h2>
+<p>I'm a software developer working on enterprise medical software. I've dabbled in game development on both mobile and PC. I've enrolled in this course to further my web development skills. I look forward to learning the skills necessary to develop truly dynamic web applications that can be used in the real world.</p>
+<h2>Random Quote</h2>
+<!-- insert random quote generation code -->
+</body>
+</html>

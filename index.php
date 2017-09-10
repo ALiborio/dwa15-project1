69fe663570a69ea017b353e3b61546c6fd570545
Adam Liborio <AdamLiborio@gmail.com>
2017-09-10 02:02:50 -0400

    separated the author from the quote so it can be styled separately

--- index.php
+++ index.php
@@ -15,17 +15,31 @@
 	<h2>Random Quote</h2>
 	<div class="quote">
 	<?php 
+	// Array of [quote,author] pairs
 	$quotes =
 	[
-		"There is nothing permanent except change. - Heraclitus",
-		"Learning never exhausts the mind. - Leonardo da Vinci",
-		"No act of kindness, no matter how small, is ever wasted. - Aesop",
-		"It is far better to be alone, than to be in bad company. - George Washington",
-		"Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better. - Samuel Beckett",
-		"Not all those who wander are lost. - J. R. R. Tolkien",
-		"Tell me and I forget. Teach me and I remember. Involve me and I learn. - Benjamin Franklin"
+		["There is nothing permanent except change.","Heraclitus"],
+		["Learning never exhausts the mind.","Leonardo da Vinci"],
+		["No act of kindness, no matter how small, is ever wasted.","Aesop"],
+		["It is far better to be alone, than to be in bad company.","George Washington"],
+		["Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better.","Samuel Beckett"],
+		["Not all those who wander are lost.","J. R. R. Tolkien"],
+		["Tell me and I forget. Teach me and I remember. Involve me and I learn.","Benjamin Franklin"]
 	];
-	echo $quotes[rand(0,count($quotes)-1)];
+
+	function getQuote(int $index){
+		global $quotes;
+		return $quotes[$index][0];
+	}
+
+	function getAuthor (int $index){
+		global $quotes;
+		return $quotes[$index][1];
+	}
+
+	$index = rand(0,count($quotes)-1);
+	echo getQuote($index);
+	echo "<div class='author'><em>- ".getAuthor($index)."</em></div>";
 	?>
 	</div>
 </div>
